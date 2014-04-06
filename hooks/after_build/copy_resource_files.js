1482fd7ab7420acfb3653272146722488e7244fd
Simon Robinson <simon@robinson.ac>
2014-04-06 15:23:17 +0530

    Updated screen size handling; various other tweaks
    - Removed target-densitydpi and instead adjusted for
    window.devicePixelRatio to ensure correct sizes across Android versions.
    - Added a title to _alert messages
    - Handled iOS camera cancel message
    - Added FastClick in an attempt to remove excruciatingly long click
    delay on iOS (@thehogfather - any better suggestions, or a way to get
    rid of the need to tap twice on iOS?)

--- hooks/after_build/copy_resource_files.js
+++ hooks/after_build/copy_resource_files.js
@@ -1,43 +1,44 @@
 #!/usr/bin/env node
- 
+
 /**
  This hook copies the resource files needed for each platform
  
  !!!***AT THE MOMENT ONLY COPIES FILES FOR ANDROID***!!!!
 */
- 
+
 var exec = require("child_process").exec,
-    path = require("path"),
-    root_dir = process.argv[2],
-    prefix = "www/res/icon/";
+	path = require("path"),
+	root_dir = process.argv[2],
+	prefix = "www/res/icon/";
 var platforms = ["android", "ios"];
 
 var android = {
-    "icon-36-ldpi.png": "drawable/icon.png",
-    "icon-48-mdpi.png": "drawable-mdpi/icon.png",
-    "icon-72-hdpi.png": "drawable-hdpi/icon.png",
-    "icon-96-xhdpi.png": "drawable-xhdpi/icon.png"
+	"icon-36-ldpi.png": "drawable/icon.png",
+	"icon-48-mdpi.png": "drawable-mdpi/icon.png",
+	"icon-72-hdpi.png": "drawable-hdpi/icon.png",
+	"icon-96-xhdpi.png": "drawable-xhdpi/icon.png"
 }, ios = {
-       
-};
+
+	};
 
 console.log("COPYING RESOURCES...");
-Object.keys(android).forEach(function (source) {
-    var sourcePath = path.join(root_dir, prefix + "android/" + source),
-        destinationPath = path.join(root_dir, "platforms/android/res/" + android[source]);
-    var command = "cp " + sourcePath + " " + destinationPath;
-    exec(command);
+Object.keys(android).forEach(function(source) {
+	var sourcePath = path.join(root_dir, prefix + "android/" + source),
+		destinationPath = path.join(root_dir, "platforms/android/res/" + android[source]);
+	var command = "cp " + sourcePath + " " + destinationPath;
+	exec(command);
 });
 
 var plugins = [
-    "org.apache.cordova.camera",
-    "org.apache.cordova.dialogs",
-    "https://github.com/devgeeks/Canvas2ImagePlugin.git",
-    "https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin.git"
+	"org.apache.cordova.camera",
+	"org.apache.cordova.dialogs",
+	"org.apache.cordova.device",
+	"https://github.com/devgeeks/Canvas2ImagePlugin.git",
+	"https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin.git"
 ];
 
-plugins.forEach(function (p) {
-    //remove the plugin 
-    console.log("installing " + p);
-    exec("phonegap plugin add " + p);
+plugins.forEach(function(p) {
+	//remove the plugin 
+	console.log("installing " + p);
+	exec("phonegap plugin add " + p);
 });
